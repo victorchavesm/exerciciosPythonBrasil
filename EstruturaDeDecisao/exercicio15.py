23b85255cc696c8e7781b3c3f7a74903ce312af0
victorchavesm <victorhcmnz@gmail.com>
2023-05-03 18:58:36 -0300

    Exercicios

--- EstruturaDeDecisao/exercicio15.py
+++ EstruturaDeDecisao/exercicio15.py
@@ -15,7 +15,7 @@ if (lado1 + lado2 < lado3) or (lado1 + lado3 < lado2) or (lado2 + lado3 < lado1)
     print('Não é um triangulo.')
 else: 
     if (lado1 == lado2) and (lado1 == lado3) :
-        triangulo = 'Equilatero'
+        triangulo = 'Equilátero'
     elif (lado1 == lado2) or (lado1 == lado3) or (lado2 == lado3):
         triangulo = 'Isósceles'
     else:
---
+++ EstruturaDeDecisao/exercicio17.py
@@ -0,0 +1,11 @@
+# Faça um Programa que peça um número correspondente a um determinado ano e 
+# em seguida informe se este ano é ou não bissexto.
+
+ano = int(input('Digite o ano para saber se é bissexto: '))
+
+if ano == 0:
+    print('Erro: Por favor, insira um ano válido.')
+elif (ano % 4) == 0 and (ano % 100) != 0 or ano % 400 == 0:
+    print(f'{ano} é bissexto.')
+else:
+    print(f'{ano} não é bissexto.')
